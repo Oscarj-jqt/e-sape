3d06e37ce93f8a34ed3856273c2ad356410a9441
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-02-12 09:48:04 +0100

    Add token expiration

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, render_template, request, jsonify
 from flask_jwt_extended import JWTManager
 from routes.auth import auth_bp 
 from routes.users import users_bp
+from datetime import timedelta
 
 
 app = Flask(__name__)
 
 
 # Configuration de la clé JWT
-app.config['JWT_SECRET_KEY'] = 'web2'
+app.config['JWT_SECRET_KEY'] = 'e-sape'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)  
 jwt = JWTManager(app)
 
 # Enregistrement des Blueprints
