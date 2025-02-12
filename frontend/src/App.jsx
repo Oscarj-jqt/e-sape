5e7e82a76ff025ad8de8f192ce60578109de3cce
Issa Abdoulaye <Issabdoulaye75@gmail.com>
2025-02-12 14:44:19 +0100

    ajout home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import {React} from "react";
 import Nav from "./components/Nav";
+import Home from "./components/Home";
 const App = () => {
     return (
         <div>
             <Nav />
+            <Home />
         </div>
     )
 }
