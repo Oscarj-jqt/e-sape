35391e9a4b44911f1bea4f6d0b24c609b214d831
Bagharia <hu.alexis@hotmail.com>
2025-02-12 14:50:44 +0100

    modification header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {React} from "react";
 import Nav from "./components/Nav";
+import "./index.css"
 const App = () => {
     return (
         <div>
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,13 +4,19 @@ import "./Nav.css";
 const Nav = () => {
     return (
         <header>
-            <h1>Issape</h1>
-            <nav>
-                <a href="#">Panier</a>
-                <a href="#">Catalogue</a>
-                <a href="#">Login</a>
-                <input type="text" placeholder="Issape" />
+            <div className="header-left">
+                <h1>E-sape</h1>
+            </div>
+            <nav className="header-center">
+                <a href="#">Lorem</a>
+                <a href="#">Lorem</a>
+                <a href="#">Lorem</a>
             </nav>
+            <div className="header-right">
+                <input type="text" placeholder="Rechercher" />
+                <button>Panier</button>
+                <button>Connexion</button>
+            </div>
         </header>
     )
 }
